61be2c52b188214c4f32010cc88f34433f80a272
SengokuGarp <garpirungu@gmail.com>
2023-10-09 09:05:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
 // Your code here
+// Helper function to create an employee record
+function createEmployeeRecord(firstName, familyName, title, payPerHour) {
+    return {
+      firstName,
+      familyName,
+      title,
+      payPerHour,
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+  
+  // Helper function to create an array of employee records
+  function createEmployeeRecords(employeeData) {
+    return employeeData.map(data => createEmployeeRecord(...data));
+  }
+  
+  // Helper function to create a time-in event
+  function createTimeInEvent(employee, timeStamp) {
+    const [date, hour] = timeStamp.split(' ');
+    employee.timeInEvents.push({
+      type: 'TimeIn',
+      date,
+      hour: parseInt(hour, 10),
+    });
+    return employee;
+  }
+  
+  // Helper function to create a time-out event
+  function createTimeOutEvent(employee, timeStamp) {
+    const [date, hour] = timeStamp.split(' ');
+    employee.timeOutEvents.push({
+      type: 'TimeOut',
+      date,
+      hour: parseInt(hour, 10),
+    });
+    return employee;
+  }
+  
+  // Helper function to calculate hours worked on a specific date
+  function hoursWorkedOnDate(employee, date) {
+    const timeIn = employee.timeInEvents.find(event => event.date === date);
+    const timeOut = employee.timeOutEvents.find(event => event.date === date);
+    
+    const hoursWorked = (timeOut.hour - timeIn.hour) / 100;
+    return hoursWorked;
+  }
+  
+  // Helper function to calculate wages earned on a specific date
+  function wagesEarnedOnDate(employee, date) {
+    const hoursWorked = hoursWorkedOnDate(employee, date);
+    return hoursWorked * employee.payPerHour;
+  }
+  
+  // Helper function to calculate total wages for an employee
+  function allWagesFor(employee) {
+    const datesWorked = employee.timeInEvents.map(event => event.date);
+    const totalWages = datesWorked.reduce((total, date) => {
+      return total + wagesEarnedOnDate(employee, date);
+    }, 0);
+    return totalWages;
+  }
+  
+  // Function to calculate total payroll for all employees
+  function calculatePayroll(employees) {
+    const totalPayroll = employees.reduce((total, employee) => {
+      return total + allWagesFor(employee);
+    }, 0);
+    return totalPayroll;
+  }
+  
